691c6f6e25de30c507518141a61f2a1f752fd537
Binod <ybinod857@gmail.com>
2025-04-30 13:43:38 +0545

    change

--- resources/views/back/pages/auth/login.blade.php
+++ resources/views/back/pages/auth/login.blade.php
@@ -1,1 +1,5 @@
-login page....
+@extends('back.layout.auth-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Page Title Here')
+@section('content')
+Page content here....
+@endsection
