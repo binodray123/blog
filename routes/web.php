4736948449c905fe49cd7ba426856faea92d3628
Binod <ybinod857@gmail.com>
2025-04-30 12:29:08 +0545

    Adding Laravel Blog Post

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/*
+* Admin Routes
+*/
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::middleware([])->group(function () {
+        Route::controller(AuthController::class)->group(function () {
+            Route::get('/login', 'loginForm')->name('login');
+            Route::get('/forgot-password', 'forgotForm')->name('forgot');
+        });
+    });
+
+    Route::middleware([])->group(function () {
+        Route::controller(AdminController::class)->group(function () {
+            Route::get('/dashboard', 'adminDashboard')->name('dashboard');
+        });
+    });
+});
